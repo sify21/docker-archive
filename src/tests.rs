e56ec21c70ea13f7d747f1532816aa2cff0f14c8
司芳源 <sify1221@gmail.com>
2022-04-07 10:37:55 +0800

    tests

--- src/tests.rs
+++ src/tests.rs
@@ -3,9 +3,21 @@ use crate::Result;
 use std::fs::OpenOptions;
 
 #[test]
-fn test_local_tar() -> Result<()> {
+fn test_url() -> Result<()> {
     let img = ImageArchive::new_from_url("postgres:13")?;
-    println!("{:?}", img);
+    assert_eq!(img.manifest.layer_tar_paths.len(), 13);
+    let mut out = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("/tmp/postgres.json")?;
+    serde_json::to_writer_pretty(&mut out, &img)?;
+    Ok(())
+}
+
+#[test]
+fn test_tar() -> Result<()> {
+    let img = ImageArchive::new_from_file("/tmp/postgres.tar")?;
     assert_eq!(img.manifest.layer_tar_paths.len(), 13);
     let mut out = OpenOptions::new()
         .write(true)
